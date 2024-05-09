51f283713c14e7694f37f30ef8edf9a274f925d7
unknown <sharjeelahmed3808@gmail.com>
2024-05-09 03:06:14 -0700

    string methods

---
+++ script.js
@@ -0,0 +1,21 @@
+// 7 primitive data types
+// string , Symbol , Number ,null,BigInt , Boolean , undefined
+// non premitive data types
+// Array , object ,function
+
+// ********string methods
+// const string = "Sharjeel herry chery";
+// console.log(string.toLocaleLowerCase());
+// console.log(string.toLocaleUpperCase());
+// console.log(string.trimEnd());
+// console.log(string.charAt(1));
+// console.log(string.length);
+// console.log(string.indexOf("h"));
+// console.log(string[0]);
+// console.log(string.slice(1, 5));
+// console.log(string.replace("h", ""));
+// console.log(string.includes("h"));
+// console.log(string.split(" "));
+// console.log(Array(string));
+
+// ****number methods
